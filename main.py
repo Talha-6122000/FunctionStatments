f6fa0b894cc14826f92786b530538267cf7d497d
Talha <bsem-f18-206@superior.edu.pk>
2021-04-14 22:21:53 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Creating a simple function
+def say_hello():
+    print("Hello")
+say_hello()
+# if Statments
+if True:
+    print("This is True")
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Welcome to the Auto Shop!')
+elif loc == 'Bank':
+    print('Welcome to the bank!')
+else:
+     print('Where are you?')
